ab6c9a2d50b4629d08642bd23b147690242c7679
Luiz Costa <luizcostaon@gmail.com>
2020-04-13 17:19:44 -0300

    Tarefa de NodeJS finalizada

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,61 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  var repository = {
+    id: uuid(),
+    title: title,
+    url: url,
+    techs: techs,
+    likes: 0
+  };
+  repositories.push(repository);
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  repositories.filter( elem => {
+    return elem.id === id
+  }).map((elem) => {
+    elem.title = title;
+    elem.url   = url;
+    elem.techs = techs;
+    
+    return response.json(elem);
+  });
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  for(var i = 0; i < repositories.length; i++){
+    if(repositories[i].id === id){
+      repository = repositories[i];
+      repositories.splice(i, 1);
+      return response.json({
+        message: 'Repositório removido com sucesso!',
+        item: repository
+      });
+    }
+  }
+  
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  repositories.filter( elem => {
+    return elem.id === id
+  }).map((elem) => {
+    elem.likes++;
+    
+    return response.json({likes: elem.likes});
+  });
 });
 
 module.exports = app;
